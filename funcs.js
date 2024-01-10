638277a9dfdae6fd7494801dd045b62d6ebbf667
SLMCreatives <sulaiman@slmcreatives.com>
2024-01-10 12:59:46 +0800

    Add Soc Med Mgt Page

--- funcs.js
+++ funcs.js
@@ -2,4 +2,21 @@ function comingSoon() {
     alert("Coming Soon!");
     console.log("Coming Soon!");
 }
+    const x = document.querySelector(".contact-form");
+    const y = document.querySelector(".contact-overlay");
+
+function toggleForm() {
+
+    if (!x.style.opacity || x.style.opacity === "0") {
+        x.style.opacity = "1", 
+        y.style.height = "100vh"
+        ;
+    } else {
+        x.style.opacity = "0",
+        y.style.height = "0vh"
+        ;
+    }
+}
+
+
 
