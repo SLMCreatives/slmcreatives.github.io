75982a14a52d3d5930d225e4eab5a53021492700
SLMCreatives <sulaiman@slmcreatives.com>
2024-01-12 01:59:33 +0800

    Attempt to form to email

--- funcs.js
+++ funcs.js
@@ -20,5 +20,33 @@ function toggleForm() {
     }
 }
 
+function onSubmit() {
+/*   Email.send({
+    SecureToken : "a0736a0e-3ef5-474d-8eb6-20dfda312748",
+    To : 'sulaiman@slmcreatives.com',
+    From : "namialus.shafiq@gmail.com",
+    Subject : "This is the subject",
+    Body : "And this is the body"
+}).then(
+  message => alert(message),
+  console.log("suceess")
+);
+} */
+
+Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "sulaiman@slmcreatives.com",
+    Password : "E76F6D91BDFA8F4F6812C8401284FCDD2F71",
+    To : 'sulaiman@slmcreatives.com',
+    From : "you@isp.com",
+    Subject : "This is the subject",
+    Body : "And this is the body"
+}).then(
+  message => alert(message)
+);
+}
+
+
+
 
 
